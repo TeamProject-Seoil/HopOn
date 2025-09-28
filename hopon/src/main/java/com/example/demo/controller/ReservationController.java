5ed0b0cb13760816c96868f1e98c2eb21e9002f5
bravo <ryan3670@naver.com>
2025-09-28 16:50:59 +0900

    Reservation에서 ReservationEntity 로 이름 변경

--- hopon/src/main/java/com/example/demo/controller/ReservationController.java
+++ hopon/src/main/java/com/example/demo/controller/ReservationController.java
@@ -1,13 +1,9 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ReservationRequestDto;
-import com.example.demo.entity.Reservation;
-import com.example.demo.entity.UserEntity;
-import com.example.demo.repository.UserRepository;
+import com.example.demo.entity.ReservationEntity;
 import com.example.demo.service.ReservationService;
-import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -21,11 +17,11 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @PostMapping
-    public ResponseEntity<Reservation> createReservation(
+    public ResponseEntity<ReservationEntity> createReservation(
             @AuthenticationPrincipal String userid, //
             @RequestBody ReservationRequestDto dto) {
 
-        Reservation saved = reservationService.createReservation(userid, dto);
+        ReservationEntity saved = reservationService.createReservation(userid, dto);
         return ResponseEntity.ok(saved);
     }
 }
--- hopon/src/main/java/com/example/demo/service/ReservationService.java
+++ hopon/src/main/java/com/example/demo/service/ReservationService.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.ReservationRequestDto;
-import com.example.demo.entity.Reservation;
+import com.example.demo.entity.ReservationEntity;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.repository.ReservationRepository;
 import com.example.demo.repository.UserRepository;
@@ -21,12 +21,12 @@ public class ReservationService {
 
     // 예약 생성
     @Transactional
-    public Reservation createReservation(String userid, ReservationRequestDto reservationRequestDto) {
+    public ReservationEntity createReservation(String userid, ReservationRequestDto reservationRequestDto) {
         UserEntity user = userRepository.findByUserid(userid)
                 .orElseThrow(() -> new IllegalStateException("USER_NOT_FOUND"));
 
         // 2) 예약 생성
-        Reservation reservation = Reservation.builder()
+        ReservationEntity reservation = ReservationEntity.builder()
                 .user(user) // ★ FK 채움
                 .routeId(reservationRequestDto.getRouteId())
                 .direction(reservationRequestDto.getDirection())
@@ -42,7 +42,7 @@ public class ReservationService {
     }
 
     // 사용자별 예약 조회
-    public List<Reservation> getReservationsByUser(UserEntity user) {
+    public List<ReservationEntity> getReservationsByUser(UserEntity user) {
         return reservationRepository.findAll()
                 .stream()
                 .filter(r -> r.getUser().getUserNum().equals(user.getUserNum()))
@@ -50,7 +50,7 @@ public class ReservationService {
     }
 
     // 예약 전체 조회 (관리자용)
-    public List<Reservation> getAllReservations() {
+    public List<ReservationEntity> getAllReservations() {
         return reservationRepository.findAll();
     }
 }
