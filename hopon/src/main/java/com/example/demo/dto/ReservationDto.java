82d86317c22dfddb511fba0ac297824d4c004f0d
Dong <xpdlf5103@naver.com>
2025-10-20 06:57:51 +0900

    ReservationDTO 부분 일부 수정

--- hopon/src/main/java/com/example/demo/dto/ReservationDto.java
+++ hopon/src/main/java/com/example/demo/dto/ReservationDto.java
@@ -6,8 +6,10 @@ public record ReservationDto(
         Long id,
         String routeId,
         String direction,
+        String boardStopId,     // ✅ 추가
         String boardStopName,
         String boardArsId,
+        String destStopId,      // ✅ 추가
         String destStopName,
         String destArsId,
         String status,
@@ -16,10 +18,18 @@ public record ReservationDto(
 ) {
     public static ReservationDto from(ReservationEntity e) {
         return new ReservationDto(
-                e.getId(), e.getRouteId(), e.getDirection(),
-                e.getBoardStopName(), e.getBoardArsId(),
-                e.getDestStopName(), e.getDestArsId(),
-                e.getStatus().name(),e.getRouteName(),e.getUpdatedAt()
+                e.getId(),
+                e.getRouteId(),
+                e.getDirection(),
+                e.getBoardStopId(),   // ✅ 매핑
+                e.getBoardStopName(),
+                e.getBoardArsId(),
+                e.getDestStopId(),    // ✅ 매핑
+                e.getDestStopName(),
+                e.getDestArsId(),
+                e.getStatus().name(),
+                e.getRouteName(),
+                e.getUpdatedAt()
         );
     }
 }
